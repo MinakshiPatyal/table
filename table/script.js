70782b970de0116421314994d67f776a525e0481
MinakshiPatyal <132878910+MinakshiPatyal@users.noreply.github.com>
2023-07-13 21:49:52 +0530

    Add files via upload

---
+++ table/script.js
@@ -0,0 +1,9 @@
+let number = parseInt(prompt("Enter the number"));
+{
+    for(let i=1; i<=10; i++){
+        var result = i*number 
+    document.write(`${number} * ${i} = ${result}` ) ;
+
+    }
+}
+
